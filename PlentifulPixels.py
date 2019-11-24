0d46fad08bd73a9e586dd23c66854869587d35b0
Rylo36 <Rylo36@users.noreply.github.com>
2019-11-24 10:45:21 -0500

    HD Colors Update and added Circles

--- PlentifulPixels.py
+++ PlentifulPixels.py
@@ -3,8 +3,8 @@ import math
 import random
 
 #Config
-windowsize = 800 #window dimensions, recommended is 1000
-resolution = 30 #size of a pixel, recommended is 10
+windowsize = 600 #window dimensions, recommended is 1000
+resolution = 10 #size of a pixel, recommended is 10
 #WARNING: The resolution CAN NOT be higher than the window size
 
 pixeloff = "black"
@@ -23,13 +23,15 @@ enablecollision = False
 
 win = GraphWin("Plentiful Pixels by Ryan Lopez", windowsize, windowsize)
 
+#Object Maker Timer
 
+timer = [0,20] #Do not touch timer[0]
 
 #Objects
 
 class Object:
     type = "None" #Square, Circle, or None
-    color = 1#Color of object, can be 1-5 depending on wanted color anything above 5 is white
+    color = [0,0,0]#Color of object, can be 1-5 depending on wanted color anything above 5 is white
     x = 0
     y = 0
     vx = 0
@@ -38,17 +40,47 @@ class Object:
 
 
 def main():
+    global timer
     #Setup
 
     CreatePixels()
     CreateRandomObject()
     CreateRandomObject()
     CreateRandomObject()
+    CreateRandomObject()
+    CreateRandomObject()
 
     while True:
+        
+        timer[0] += 1
+
+        if(timer[0] >= timer[1]):
+            timer[0] = 0
+            CreateRandomObject()
+        ObjectCleaner() #Removes objects that are off the screen
         Physics()
         Render()
+
+def ObjectCleaner():
+    global objects
+
+    bounds = Object.size
+
+    cur = len(objects) - 1
+    while(cur >= 0):
+        if(objects[cur].x < 0 - bounds or objects[cur].y < 0 - bounds):
+            DeleteObject(cur)
+        elif(objects[cur].x > windowsize + bounds or objects[cur].y > windowsize + bounds):
+            DeleteObject(cur)
+        cur -= 1
+
+def DeleteObject(cur):
+    global objects
+    del objects[cur]
+
    
+
+
 def Physics():
     global objects
     global enablecollision
@@ -72,13 +104,15 @@ def CreateRandomObject():
     objects.append(Object())
 
     speed = 30
-
-    objects[cur].type = "Square"
+    if(random.randint(1,2) == 1):
+        objects[cur].type = "Circle"
+    else:
+        objects[cur].type = "Circle"
     objects[cur].x = random.randint(1,windowsize)
     objects[cur].y = random.randint(1,windowsize)
     objects[cur].vx = random.randint(-speed,speed)
     objects[cur].vy = random.randint(-speed,speed)
-    objects[cur].color = random.randint(1,4)
+    objects[cur].color = [random.randint(1,255),random.randint(1,255),random.randint(1,255)]
     
 def CreatePixels():
     global pixel
@@ -132,19 +166,23 @@ def Render():
     while(c < windowsize):
         while(d <= windowsize):
             
-            rgb = round(Blend(c - (resolution / 2),d - (resolution / 2)))
-            if(rgb <= 0):
-                pixel[slot].setFill(pixeloff)
-            if(rgb == 1):
-                pixel[slot].setFill("red")
-            if(rgb == 2):
-                pixel[slot].setFill("blue")
-            if(rgb == 3):
-                pixel[slot].setFill("green")
-            if(rgb == 4):
-                pixel[slot].setFill("yellow")
-            if(rgb >= 5):
-                pixel[slot].setFill("white")
+            rgb = [0,0,0]
+            rgb[0] = round((Blend(c,d,0) + Blend(c - (resolution / 2),d - (resolution / 2),0) + Blend(c - resolution,d - resolution,0) + Blend(c,d - resolution,0) + Blend(c - resolution,d,0)) / 5)
+            rgb[1] = round((Blend(c,d,1) + Blend(c - (resolution / 2),d - (resolution / 2),1) + Blend(c - resolution,d - resolution,1) + Blend(c,d - resolution,1) + Blend(c - resolution,d,1)) / 5)
+            rgb[2] = round((Blend(c,d,2) + Blend(c - (resolution / 2),d - (resolution / 2),2) + Blend(c - resolution,d - resolution,2) + Blend(c,d - resolution,2) + Blend(c - resolution,d,2)) / 5)
+            pixel[slot].setFill(color_rgb(rgb[0],rgb[1],rgb[2]))
+            #if(rgb <= 0):
+            #    pixel[slot].setFill(pixeloff)
+            #if(rgb == 1):
+            #    pixel[slot].setFill("red")
+            #if(rgb == 2):
+            #    pixel[slot].setFill("blue")
+            #if(rgb == 3):
+            #    pixel[slot].setFill("green")
+            #if(rgb == 4):
+            #    pixel[slot].setFill("yellow")
+            #if(rgb >= 5):
+            #    pixel[slot].setFill("white")
             slot += 1
             d += resolution
             b += 1
@@ -160,25 +198,32 @@ def Present(x = 0,y = 0,cur = 1):
     ox = objects[cur].x
     oy = objects[cur].y
     r = resolution
+    pi = math.pi
+    distance = math.sqrt(((abs(ox -x)*(abs(ox -x)) + (abs(oy - y)) * (abs(oy - y)))))
 
     if(objects[cur].type == "Square"):
-        if(x < ox + size and x > ox - size and y < oy + size and y > oy - size):
+        if(x <= ox + size and x >= ox - size and y <= oy + size and y >= oy - size):
+            return True
+        else:
+            return False
+    elif(objects[cur].type == "Circle"):
+        if(distance <= size):
             return True
         else:
             return False
     elif(objects[cur].type == "None" or objects[cur] == None):
         return False
 
-def Blend(x = 0, y = 0):
+def Blend(x = 0, y = 0,mode = 0):
     global objects
     color = 0
     cur = 0
     while(cur < len(objects)):
         if(Present(x,y,cur) == True):
             if(color == 0):
-                color = objects[cur].color
+                color = objects[cur].color[mode]
             else:
-                color = (objects[cur].color + color) / 2
+                color = (objects[cur].color[mode] + color[mode]) / 2
         cur += 1
             
     return color
