9ac5cad87df97197b107157bb8b78b67b86d2b44
Rylo36 <Rylo36@users.noreply.github.com>
2019-11-25 17:34:16 -0500

    Feature Update
    Added Manual Creation of Objects
    Added key controls
    ....

--- PlentifulPixels.py
+++ PlentifulPixels.py
@@ -2,45 +2,73 @@ from graphics import *
 import math
 import random
 
-#Config
+#Config <START OF CONFIG> --------------------------------------------------------------------
+allowphysics = True #Disable physics if you would like by setting it to false
+enableobjectmaker = False #Allows the user to create objects manually 
+automakeobjects = True #Randomly Generate Objects, will not exceed the object max #
 windowsize = 500 #window dimensions, recommended is 500
-resolution = 20 #size of a pixel, recommended is 20
+resolution = 15 #size of a pixel, recommended is 20
 objectstohave = 10#Number of objects to have on the canvas at all times
 #WARNING: The resolution CAN NOT be higher than the window size
 
+#Background
 pixeloff = "black"
 
 #Graphics Engine
-nec = windowsize / resolution
-pixel = []
-enablesquare = True
+enablesquare = True #Who doesn't like Squares?
 enablecircle = True
-colorsupport = True
+colorsupport = True #No real practical use, everyone likes color
 
 #Color Config
-maxintensity = 200
-minintensity = 100
-deepeffect = 2 #Only change this if you know what your doing, i recommend 2
+maxintensity = 250 #max rgb value
+minintensity = 100 #min rgb value
+deepeffect = 2 #This changes how quickly an object changes in opacity (the higher, the quicker), I recommend 2
 
 #Physics Engine
-objects = []
-raw = []
+
 enablecollision = False #Not currently working (planned to be)
-
-#Performance
-fasterplease = False #Set it to true if you experience slow render times
-lowres = False #Set it to true if you must, but i really recommend the hd colors if you can
-
-win = GraphWin("Plentiful Pixels by Ryan Lopez", windowsize, windowsize)
-
-#Object Maker Timer
-
-timer = [-1,10] #Do not touch timer[0]
 
 #Objects
 
 minsize = 30
 maxsize = 50
+
+#Performance
+fasterplease = False #Set it to true if you experience slow render times
+lowres = False #Set it to true if you must, but i really recommend the hd colors if you can
+
+# Keybinds:
+# s = decrease the size of objects created through the create tool
+# s + shift = increase the size of objects created through the create tool
+# z = undo (currently it can only go back one step)
+# esc = delete all objects
+# p = toggle physics
+# t = toggle between shapes
+# r = toggle random generation of shapes
+
+
+#Begin Setup <END OF CONFIG> ----------------------------------------------------------------------------------
+
+objects = []
+raw = []
+nec = windowsize / resolution  #Please dont touch
+pixel = []
+
+win = GraphWin("Plentiful Pixels by Ryan Lopez", windowsize, windowsize)
+
+notify = Text(Point(windowsize / 2, windowsize / 20), "Someone forgot to add text")
+notifytimer = [-1,120]
+notify.setSize(30)
+
+notifymore = Text(Point(windowsize / 2, windowsize / 10), "Someone forgot to add data")
+notifymore.setSize(15)
+notify.setFill("white")
+
+#Object Maker Timer, please ignore
+lastplaced = -1
+timer = [0,10] #Do not touch timer[0]
+
+
 
 class Object:
     type = "None" #Square, Circle, or None
@@ -49,21 +77,121 @@ class Object:
     y = 0
     vx = 0
     vy = 0
+    rot = 0 #AKA Rotation
     size = 50#This will never change (unless done here)
 
+class Tool:
+    type = "Square"
+    size = 50
+    color = [0,0,0]
+    vx = 0
+    vy = 0
 
-def main():
-    global timer
+undocounter = 0
+tool = Tool()
+toollog = []
+
+def leftclick(event):
+    global toollog
+    global tool
+    global lastplaced
+    toollog.append(len(objects))
+    CreateObject(event.x,event.y,tool.size,tool.type)
+
+def rightclick(event):
+    global lastplaced
+    if(lastplaced == -1):
+        print("Leftclick does nothing at all")
+    else:
+        objects[lastplaced].color = [objects[lastplaced].color[0] ]
+
+def realtime():
+    global notify
+    global notifymore
+    global tool
+    global notifytimer
+    global allowphysics
+    global win
     global objectstohave
     global objects
+    global lastplaced
+    global automakeobjects
+    global toollog
+    global undocounter
+
+    if(notifytimer[0] < -1):
+        notifytimer[0] = -1
+    elif(notifytimer[0] == 0):
+        notifytimer[0] = -1
+        notify.undraw()
+        notifymore.undraw()
+    elif(notifytimer[0] > 0):
+        notifytimer[0] -= 1
+
+    checkString = win.checkKey()
+    if(checkString != None):
+        if(checkString == "Escape"): 
+            objects = []
+            toollog = []
+            Notify("Objects Cleared"," ","yellow")
+        if(checkString == "p"): 
+            if(allowphysics == True): 
+                allowphysics = False
+                Notify("Physics Disabled","Press p to renable","red")
+            else: 
+                allowphysics = True
+                Notify("Physics Enabled","Press p to disable","green")
+        if(checkString == "r"): 
+            if(automakeobjects == True): 
+                automakeobjects = False
+                Notify("Shape Generation Disabled","Press r to renable","red")
+            else: 
+                automakeobjects = True
+                Notify("Shape Generation Enabled","Press r to disable","green")
+        if(checkString == "s"):
+                tool.size += 10
+                Notify("Size Increased",tool.size,"green")
+        if(checkString == "S"):
+                tool.size -= 10
+                Notify("Size Decreased",tool.size,"red")
+        if(checkString == "z"):
+                if(len(toollog) > 0):
+                    undocounter += 1
+                    Notify("Actions Undone:",undocounter,"yellow")
+                else:
+                    Notify("Error","No Actions to Undo","red")
+        if(checkString == "t"):
+                if(tool.type == "Square"):
+                    tool.type = "Circle"
+                    Notify("Shape Selected:",tool.type,"yellow")
+                else: 
+                    tool.type = "Square"
+                    Notify("Shape Selected:",tool.type,"yellow")
+
+        if(undocounter > len(toollog) -1): undocounter = len(toollog) - 1
+            
+
+def main():
+    global notify
+    global notifymore
+    global tool
+    global notifytimer
+    global allowphysics
+    global win
+    global objectstohave
+    global objects
+    global undocounter
+    global toollog
     #Setup
+    win.bind('<Button-1>', leftclick)
+    win.bind('<Button-3>', rightclick)
 
     CreatePixels()
 
     a = 0
 
     while(a < objectstohave):
-        CreateRandomObject()
+        CreateObject()
         a += 1
 
     while True:
@@ -72,14 +200,41 @@ def main():
 
         #    if(timer[0] >= timer[1]):
         #        timer[0] = 0
-        #        CreateRandomObject()
+        #        CreateObject()
 
-        if(len(objects) < objectstohave):
-            CreateRandomObject()
+        if(len(objects) < objectstohave and automakeobjects == True):
+            CreateObject()
 
+
+        if(undocounter > len(toollog) -1): undocounter = len(toollog) - 1
+
+        while(undocounter > 0):
+            a = len(toollog) - 1
+            DeleteObject(toollog[a])
+            del toollog[a]
+            undocounter -= 1
+            
+                    
+        
         ObjectCleaner() #Removes objects that are off the screen
-        Physics()
+        if(allowphysics == True): Physics()
         Render()
+
+def Notify(msg = "None",submsg = "none", color = "white", time = -1):
+    global notify
+    global notifymore
+    global win
+    global notifytimer
+    notifymore.setText(submsg)
+    notify.setTextColor(color)
+    notify.setText(msg)
+    notifymore.setTextColor(color)
+    if(notifytimer[0] == -1):
+        notify.draw(win)
+        notifymore.draw(win)
+    if(time == -1): notifytimer[0] = notifytimer[1]
+    else: notifytimer = time
+
 
 def ObjectCleaner():
     global objects
@@ -96,7 +251,14 @@ def ObjectCleaner():
 
 def DeleteObject(cur):
     global objects
+    global toollog
     del objects[cur]
+    
+    a = len(toollog)
+    while(a > 0):
+        if(toollog[a] == cur):
+            del toollog[a]
+        a -= 1
 
    
 
@@ -115,23 +277,25 @@ def Physics():
             objects[a].y += objects[a].vy
         a += 1
 
-def CreateRandomObject():
+def CreateObject(x = random.randint(1,windowsize),y = random.randint(1,windowsize),size = random.randint(minsize,maxsize),type = "Random"):
     global objects
     global maxintensity
     global minintensity
-
+    
     cur = len(objects)
 
     objects.append(Object())
 
     speed = 60
-    if(random.randint(1,2) == 1):
-        objects[cur].type = "Square"
-    else:
-        objects[cur].type = "Circle"
-    objects[cur].size = random.randint(minsize,maxsize)
-    objects[cur].x = random.randint(1,windowsize)
-    objects[cur].y = random.randint(1,windowsize)
+    if(type == "Random"):
+        if(random.randint(1,2) == 1):
+            objects[cur].type = "Square"
+        else:
+            objects[cur].type = "Circle"
+    else: objects[cur].type = type
+    objects[cur].size = size
+    objects[cur].x = x
+    objects[cur].y = y
     objects[cur].vx = random.randint(-speed,speed)
     objects[cur].vy = random.randint(-speed,speed)
     if(objects[cur].vx == 0):
@@ -168,8 +332,9 @@ def CreatePixels():
 
 def ClearRender():
     global pixel
+    
 
-    #Notice how the pixel is NOT undrawn before it is deleted. Doing so would cause the window to flash every frame update
+    
     a = len(pixel) - 1
     while(a > 1):
         #pixel[a].undraw()
@@ -204,6 +369,7 @@ def Render():
                 rgb[1] = rgb[0]
                 rgb[2] = rgb[0]
 
+            realtime()
             pixel[slot].setFill(color_rgb(rgb[0],rgb[1],rgb[2]))
             slot += 1
             d += resolution
